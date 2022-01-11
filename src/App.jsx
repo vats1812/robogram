552ab3ecb315b2f22491fec02df1313629197d52
vats1812 <68377043+vats1812@users.noreply.github.com>
2022-01-11 22:06:04 +0530

    full project

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { FaRobot } from "react-icons/fa";
+import "./App.css";
+function App() {
+  const [name, setName] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  };
+  
+  return (
+    <div className="app">
+      <nav>
+        <h1 className="center">
+          <FaRobot /> RoboGram
+        </h1>
+      </nav>
+      <section>
+        <div className="container">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="search center">
+              <input
+                type="text"
+                placeholder="Enter Something....."
+                onChange={handleChange}
+              />
+              <p>Write something and hit ENTER !!</p>
+            </div>
+          </form>
+          <div className="robot center">
+            <img src={`https://robohash.org/${name}.png`} />
+            <p>{name}</p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
